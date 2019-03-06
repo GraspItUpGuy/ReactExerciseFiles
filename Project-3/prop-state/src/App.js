0409089328864b4b7901c59c51ab2a9c785441d1
Puneet <birmaan.puneet19@gmail.com>
2019-03-06 22:26:43 +0530

    props
    Signed-off-by: Puneet <birmaan.puneet19@gmail.com>

--- Project-3/prop-state/src/App.js
+++ Project-3/prop-state/src/App.js
@@ -6,11 +6,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+       
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          
           <a
             className="App-link"
             href="https://reactjs.org"
@@ -19,10 +21,40 @@ class App extends Component {
           >
             Learn React
           </a>
+          
         </header>
+        <Parent />
       </div>
     );
   }
 }
 
+class Parent extends Component{
+  render(){
+    return(
+      <div>
+        <h2>Just some info</h2>
+        <Cars  msg = "Cars are very very cool" model= "2343235" coolCars = {this.props.cars}/>
+      </div>
+    )
+  }
+}
+
+Parent.defaultProps = {
+  cars : ["BMW " , "AUDI " , "TOYOTA ", "CITY "] 
+}
+
+class Cars extends Component{
+  render(){
+    console.log(this.props)
+    return(
+      <h3>I am from cars component
+      <p>{this.props.msg}</p>
+      <p> {this.props.coolCars}</p> 
+      <p>{this.props.model} </p>
+      </h3>
+      
+    )
+  }
+}
 export default App;
