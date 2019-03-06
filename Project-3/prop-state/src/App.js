1a0ce1eb53c5d4ef42f30728d7f116c88486d83b
Puneet <birmaan.puneet19@gmail.com>
2019-03-07 00:02:31 +0530

    states
    Signed-off-by: Puneet <birmaan.puneet19@gmail.com>

--- Project-3/prop-state/src/App.js
+++ Project-3/prop-state/src/App.js
@@ -7,25 +7,21 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-       
-        <header className="App-header">
+        <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <h3>prop number is : {this.props.propNumber}</h3>
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <h3>prop number is : {this.props.propNumber}</h3>
         <h3>prop number is : {this.props.propString}</h3>
         <h3>prop number is : {this.props.propObject.obj1}</h3>
-       
-        </header>
         <Parent />
       </div>
     );
   }
 }
-
-// PropTypes now has been included into a seperate package in npm => 'prop-types'
-// install it seperately from npmjs.org
 
 App.propTypes = {
   propObject: PropTypes.object,
@@ -44,37 +40,52 @@ App.defaultProps = {
 
 }
 
-class Parent extends Component{
+
+
+
+class Parent extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      cars: ['s-BMW', 's-MERC', 's-City', 's-Audi']
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(){
+    this.setState( {cars: this.state.cars.reverse()});
+  }
+
   render(){
     return(
       <div>
-        <h2>Just some info</h2>
-        <Cars  msg = "Cars are very very cool" model= "2343235" coolCars = {this.props.cars}/>
+        <h2 onClick={this.handleClick}>Just some info</h2>
+        <Cars msg="cars are cool" model="34765" coolCars={this.state.cars}/>
       </div>
-    )
+    );
   }
 }
-
-
-
-
 
 
 Parent.defaultProps = {
-  cars : ["BMW " , "AUDI " , "TOYOTA ", "CITY "] 
+  cars: ['BMW', 'MERC', 'City', 'Audi']
 }
 
-class Cars extends Component{
+class Cars extends Component {
   render(){
-    console.log(this.props)
+    console.log(this.props);
     return(
-      <h3>I am from cars component
-      <p>{this.props.msg}</p>
-      <p> {this.props.coolCars}</p> 
-      <p>{this.props.model} </p>
-      </h3>
-      
-    )
+      <div>
+        <h3> I am from cars component</h3>
+        <p>{this.props.msg}</p>
+        <p>{this.props.model}</p>
+        <div>{this.props.coolCars.map((item, i) => {
+            return <p key={i}>{item}</p> ;
+          })}</div>
+      </div>
+    );
   }
 }
+
 export default App;
