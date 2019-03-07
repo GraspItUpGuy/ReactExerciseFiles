f96acf69820bb12da967bd0586773a536080ee55
Puneet <birmaan.puneet19@gmail.com>
2019-03-07 22:41:28 +0530

    building basic app
    Signed-off-by: Puneet <birmaan.puneet19@gmail.com>

--- Project-5/basics/src/app.js
+++ Project-5/basics/src/app.js
@@ -6,7 +6,12 @@ import ReactDOM from 'react-dom';
 class App extends Component{
     render(){
         return(
-            
+            // JSX goes here
+          <div>
+              Cool React App
+          </div>  
         )
     }
 }
+
+ReactDOM.render(<App />, document.getElementById('root'))
