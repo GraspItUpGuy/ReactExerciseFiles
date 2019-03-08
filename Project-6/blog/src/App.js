bfc33fd82290600c43eac980cd4f3c4307e3cfce
Puneet <birmaan.puneet19@gmail.com>
2019-03-08 14:07:25 +0530

    moving navbar to seperate component
    Signed-off-by: Puneet <birmaan.puneet19@gmail.com>

--- Project-6/blog/src/App.js
+++ Project-6/blog/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './NavBar'
 
 class App extends Component {
   
@@ -10,33 +11,7 @@ class App extends Component {
     }
     return (
       <div>
-      {/* <!-- Navigation --> */}
-      <nav classNameName="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
-        <div className="container">
-          <a className="navbar-brand" href="index.html">Start Bootstrap</a>
-          <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-            Menu
-            <i className="fas fa-bars"></i>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarResponsive">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <a className="nav-link" href="index.html">Home</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="about.html">About</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="post.html">Sample Post</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="contact.html">Contact</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    
+      <NavBar />
       {/* <!-- Page Header --> */}
       <header className="masthead" style={headerstyle}>
         <div className="overlay"></div>
@@ -44,8 +19,8 @@ class App extends Component {
           <div className="row">
             <div className="col-lg-8 col-md-10 mx-auto">
               <div className="site-heading">
-                <h1>Clean Blog</h1>
-                <span className="subheading">A Blog Theme by Start Bootstrap</span>
+                <h1>GraspItUpGuy</h1>
+                <span className="subheading">A Story-teller by heart...</span>
               </div>
             </div>
           </div>
