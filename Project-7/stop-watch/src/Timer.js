467790d3e03eff324855a27eb6f896002799fe0c
Puneet <birmaan.puneet19@gmail.com>
2019-03-09 10:34:42 +0530

    layout for the timer app
    Signed-off-by: Puneet <birmaan.puneet19@gmail.com>

--- Project-7/stop-watch/src/Timer.js
+++ Project-7/stop-watch/src/Timer.js
@@ -4,7 +4,9 @@ class Timer extends Component{
     render(){
         return(
             <div>
-              
+              <p>You have been in this site since : </p> <br />
+              <span> 55.83 </span>
+              <p> Seconds.</p>
             </div>
         )
     }
