0af9c0ecbbbc02a0b133147b70c806ec8deb7a77
Puneet <birmaan.puneet19@gmail.com>
2019-03-09 16:49:16 +0530

    course sales component
    Signed-off-by: Puneet <birmaan.puneet19@gmail.com>

---
+++ Project-8/course-purchase-app/src/CourseSales.js
@@ -0,0 +1,15 @@
+import React , { Component} from 'react'
+
+class CourseSales extends Component{
+    render(){
+        return(
+            <div>
+             I am a CourseSales Component
+            </div>
+        )
+    }
+}
+
+
+
+export default CourseSales;
