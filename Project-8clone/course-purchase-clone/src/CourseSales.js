cf7783b6a8764e1f03742178b0660810cddebd19
Puneet <birmaan.puneet19@gmail.com>
2019-03-10 17:05:30 +0530

    same bug encountered
    Signed-off-by: Puneet <birmaan.puneet19@gmail.com>

--- Project-8clone/course-purchase-clone/src/CourseSales.js
+++ Project-8clone/course-purchase-clone/src/CourseSales.js
@@ -3,6 +3,7 @@ import React, {Component} from 'react'
 class CourseSales extends Component{
 
      CartTotal(price){
+        console.log('CartTotal function working')
          this.setState = { CartTotal : this.CartTotal + price}
      }
 
@@ -18,17 +19,51 @@ class CourseSales extends Component{
     render(){
         console.log(this.props.courseName)
         var courseName = this.props.courseName.map((course , i)=>{
-            return "to be filled with course class"
+            return <Course name ={courseName.name}  price = {courseName.price}
+                     CartTotal = {this.CartTotal} active={this.state.active}
+            />
         })
         return(
             <div>
             <h2>Buy Courses here : </h2>
-
-
+                 {courseName}
+                 <p>total : {this.CartTotal} </p>
+           
             </div>
         )
     }
 }
+
+class Course extends Component { 
+    
+    Clicker(){
+        console.log('clicker function working')
+        var active = !(this.state.active)
+        this.setState={ active : active}
+        this.props.CartTotal(active ? this.props.price : -this.props.price)
+
+    }
+     
+
+    constructor(props){
+        super(props)
+
+        this.setState = { 
+            active : true,
+        }
+        this.Clicker = this.Clicker.bind(this)
+    }
+    
+    render(){
+        return(
+            <div>
+           <p onClick = {this.Clicker}> {this.props.name} <br/> {this.props.price}</p>
+            </div>
+        )
+    }
+}
+
+
 
 
 export default CourseSales;
