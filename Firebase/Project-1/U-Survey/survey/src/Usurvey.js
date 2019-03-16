dec6ac582b47ed6b9a1aa51114d428a2aba0dd53
Puneet <birmaan.puneet19@gmail.com>
2019-03-16 22:13:26 +0530

    firebase initialisation done
    Signed-off-by: Puneet <birmaan.puneet19@gmail.com>

--- Firebase/Project-1/U-Survey/survey/src/Usurvey.js
+++ Firebase/Project-1/U-Survey/survey/src/Usurvey.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react'
+const firebase = require('firebase')
+const uuid = require('uuid')
+
+var config = {
+    apiKey: "AIzaSyD0at8LzGZR2BKgB__yrTvrpXV_OWDeZ38",
+    authDomain: "u-survey-ab057.firebaseapp.com",
+    databaseURL: "https://u-survey-ab057.firebaseio.com",
+    projectId: "u-survey-ab057",
+    storageBucket: "u-survey-ab057.appspot.com",
+    messagingSenderId: "705561358082"
+  };
+  firebase.initializeApp(config);
 
 class Usurvey extends Component{
     render(){
         return(
-            <h1> I am Usurvey Component.  Nice to meet you !!!</h1>
+            <h1> I am U-Survey Component.  Nice to meet you !!!</h1>
         )
     }
 }
