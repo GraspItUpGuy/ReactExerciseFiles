d848c46901cde721edfa921ad964aff96f978779
Puneet <birmaan.puneet19@gmail.com>
2019-03-16 22:21:53 +0530

    Reason for  exposed API key added
    Signed-off-by: Puneet <birmaan.puneet19@gmail.com>

--- Firebase/Project-1/U-Survey/survey/src/Usurvey.js
+++ Firebase/Project-1/U-Survey/survey/src/Usurvey.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 const firebase = require('firebase')
 const uuid = require('uuid')
 
+// initializing the firebase => copied from the  docs
 var config = {
     apiKey: "AIzaSyD0at8LzGZR2BKgB__yrTvrpXV_OWDeZ38",
     authDomain: "u-survey-ab057.firebaseapp.com",
@@ -11,8 +12,20 @@ var config = {
     messagingSenderId: "705561358082"
   };
   firebase.initializeApp(config);
+  // firebase initialisation ends
+  // API keys exposed but it will not create any problem as it is just a test app
+
+
+
 
 class Usurvey extends Component{
+    constructor(props){
+        super(props);
+
+        this.state ={
+            uid : uuid.v1()
+        }
+    }
     render(){
         return(
             <h1> I am U-Survey Component.  Nice to meet you !!!</h1>
