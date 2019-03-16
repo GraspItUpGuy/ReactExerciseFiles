33cb79dc87418a1e1c0b22d06d4caab04a8d08c8
Puneet <birmaan.puneet19@gmail.com>
2019-03-16 18:16:59 +0530

    setting Usurvey component
    Signed-off-by: Puneet <birmaan.puneet19@gmail.com>

---
+++ Firebase/Project-1/U-Survey/survey/src/Usurvey.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Usurvey extends Component{
+    render(){
+        return(
+            <h1> I am Usurvey Component.  Nice to meet you !!!</h1>
+        )
+    }
+}
+
+
+export default Usurvey ;
+
