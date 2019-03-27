6132ed295dc2ad35414dada55c0c9f7af78c791d
Puneet <birmaan.puneet19@gmail.com>
2019-03-27 22:48:13 +0530

    create Auth-App=structure
    Signed-off-by: Puneet <birmaan.puneet19@gmail.com>

---
+++ Firebase/Firebase-Login/firebase-login/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Auth from './Auth'
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        
+        <Auth />
+        
+      </div>
+    );
+  }
+}
+
+export default App;
