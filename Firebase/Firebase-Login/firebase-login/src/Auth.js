f6596a26dbb0c824c41942a11c0ee73924f17835
Puneet <birmaan.puneet19@gmail.com>
2019-03-27 22:50:33 +0530

    configure firebase
    Signed-off-by: Puneet <birmaan.puneet19@gmail.com>

--- Firebase/Firebase-Login/firebase-login/src/Auth.js
+++ Firebase/Firebase-Login/firebase-login/src/Auth.js
@@ -1,4 +1,18 @@
 import React, { Component } from 'react';
+var firebase = require('firebase')
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCn8n5tWJfgeNPmEjMTUplKlRCVSl3D4dE",
+    authDomain: "fir-authproject-5a13d.firebaseapp.com",
+    databaseURL: "https://fir-authproject-5a13d.firebaseio.com",
+    projectId: "fir-authproject-5a13d",
+    storageBucket: "",
+    messagingSenderId: "756426620627"
+  };
+  firebase.initializeApp(config);
+
+
 
 class Auth extends Component{
     render(){
